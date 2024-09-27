68fafc214a2b24920375ec34cd4efbbcc6467216
byung123 <byung14512@naver.com>
2024-09-27 17:52:04 +0900

    9.27 학습 추가

--- src/main/java/com/study/deploy_back/controller/TestController.java
+++ src/main/java/com/study/deploy_back/controller/TestController.java
@@ -2,6 +2,7 @@ package com.study.deploy_back.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class TestController {
     @Value("${client.address}")
     private String clientAddress;
 
+    @CrossOrigin(originPatterns = "*")
     @GetMapping("/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok(clientAddress + "테스트!!");
